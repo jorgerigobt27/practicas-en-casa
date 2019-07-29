748924bcc9a2bcd178c11ec325ccf57c81381235
EDWGIOR <EDWGIOR@192.168.0.7>
2019-07-28 21:26:19 -0300

    collections

---
+++ Practica/src/JavaStandard/Clase8/Collections.java
@@ -0,0 +1,57 @@
+package JavaStandard.Clase8;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
+public class Collections {
+
+    public static void main(String args[]) {
+//HashSet
+//        HashSet<String> hashSet = new HashSet<String>();
+//        hashSet.add("element1");
+//        hashSet.add("element3");
+//        hashSet.add("element2");
+//        System.out.println(hashSet);
+//        hashSet.remove("element1");
+//        System.out.println(hashSet);
+
+//TreeSet
+//        TreeSet<String> treeSet = new TreeSet<String>();
+//        treeSet.add("element1");
+//        treeSet.add("element3");
+//        treeSet.add("element2");
+//        System.out.println(treeSet);
+//        treeSet.remove("element2");
+//        System.out.println(treeSet);
+//linkedHashSet
+//        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<String>();
+//        linkedHashSet.add("element3");
+//        linkedHashSet.add("element1");
+//        linkedHashSet.add("element2");
+//        System.out.println(linkedHashSet);
+//        linkedHashSet.remove("element1");
+//        System.out.println(linkedHashSet);
+//ArrayList
+//        ArrayList<String> arrayList = new ArrayList<String>();
+//        arrayList.add("element2");
+//        arrayList.add("element1");
+//        arrayList.add("element3");
+//        arrayList.add("element3");
+//        System.out.println(arrayList);
+//        arrayList.remove("element3");
+//        System.out.println(arrayList);
+
+        LinkedList<String> linkedList = new LinkedList<String>();
+        linkedList.add("element3");
+        linkedList.add("element1");
+        linkedList.add("element4");
+        System.out.println(linkedList);
+        linkedList.remove("element3");
+        System.out.println(linkedList);
+
+    }
+
+}
